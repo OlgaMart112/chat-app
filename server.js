c09e6622062153050f9f3aa27ec60c7b01d81897
OlgaMart112 <opam112@ukr.net>
2019-10-19 13:57:21 +0300

    changed

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 
-const WebSocket = require('ws');
+'use strict';
 
-const server = new WebSocket.Server({ port : 3000 });
+const express = require('express');
+const SocketServer = require('ws').Server;
+const path = require('path');
 
-server.on('connection', ws => {
-    ws.on('message', message => {
-      server.clients.forEach((client) => {
-        if(client.readyState === WebSocket.OPEN){
-           console.dir(message)
-            client.send(message);
-        }
-      })
-    });
-    ws.send("Welcome");
-})
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
 
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const wss = new SocketServer({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('close', () => console.log('Client disconnected'));
+});
+
+setInterval(() => {
+  wss.clients.forEach((client) => {
+    client.send(new Date().toTimeString());
+  });
+}, 1000);
